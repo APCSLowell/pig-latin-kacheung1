dcf88d65b78399001242b3f2cfae721a2d355dfd
kacheung1 <143028546+kacheung1@users.noreply.github.com>
2024-01-26 22:58:03 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -1,12 +1,32 @@
-public void setup() 
-{
-  String[] lines = {"beast","dough","happy","question","star","three","eagle","try"};
-  System.out.println("there are " + lines.length + " lines");
-  for (int i = 0 ; i < lines.length; i++) 
-  {
-    System.out.println(pigLatin(lines[i]));
-  }
-}
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class PigLatin {
+    
+    public void tester() {
+        // String[] lines = loadStrings("words.txt");
+        String[] lines = new String[8]; 
+        try{
+            File myFile = new File("words.txt");
+            Scanner myReader = new Scanner(myFile);
+            int counter = 0;
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                lines[counter] = data;
+                counter++;
+            }
+            myReader.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+	    System.out.println("there are " + lines.length + " lines");
+	    for (int i = 0 ; i < lines.length; i++) {
+	        System.out.println(pigLatin(lines[i]));
+	    }
+    }
 
 public int findFirstVowel(String sWord)
 //precondition: sWord is a valid String of length greater than 0.
